40cd29f86881a654f3475ed00aa27b8cedce7096
Güney Köse <guney.kose@outlook.com>
2022-01-30 17:57:47 +0300

    create actions in view controller

--- getsome/ViewController.swift
+++ getsome/ViewController.swift
@@ -8,17 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    var numberOfGlasses = 0
+    var numberOfGlassesLeft = 0
+    var progress: Float = 0
 
-    @IBAction func reminderPressed(_ sender: UIButton) {
-        
-    }
     
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    @IBAction func drinkButtonPressed(_ sender: UIButton) {
+        numberOfGlasses += 1
+        numberOfGlassesLeft -= 1
+        progressBar.progress += progress
+        progressLabel.text = "you drank \(numberOfGlasses) of glasses of water today."
+        goalLabel.text = "\(numberOfGlassesLeft) glasses to go!"
+    }
 }
 
